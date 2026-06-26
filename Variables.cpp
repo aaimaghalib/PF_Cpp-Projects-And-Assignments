088d1509e3916b97992616f3cab13d886539127a
aaimaghalib <aaimaghalib24@gmail.com>
2026-06-26 06:23:00 +0500

    Assignment_#01.cpp
    "A program which takes 2 integers as input from user and prints their sum, product, difference, division and remainder."

---
+++ Variables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int x=10,y= 25,sum,diff,prod,div,quo;
+	sum = a + b;
+	diff = b - a;
+	prod = c * a;
+	div = b / a;
+	quo = b % a;
+	cout<<"The sum of "<<a<< " and "<<b<<" is "<<sum<<endl;
+	cout<<"The difference of "<<b<< " and "<<a<<" is "<<diff<<endl;
+	cout<<"The product of "<<a<< " and "<<c<<" is "<<prod<<endl;
+	cout<<"The division of "<<b<< " and "<<a<<" is "<<div<<endl;
+	cout<<"The remainder of "<<b<< " and "<<a<<" is "<<quo<<endl;
+	cout<<"The sum of "<<a<< " and "<<b<<" is "<<sum<<endl;
+	return 0;
+}
