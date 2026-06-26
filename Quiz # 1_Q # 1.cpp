4730ba7141052ed0e95e7ef444802e98818d0c56
aaimaghalib <aaimaghalib24@gmail.com>
2026-06-26 05:30:04 +0500

    Add files via upload
    A program which takes 2 integers as input from user and prints their sum, product, difference, product, division and remainder.

---
+++ Quiz # 1_Q # 1.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+using namespace std; 
+int main()
+ { 
+int num1, num2; 
+cout << "Enter first number: "; 
+cin >> num1; 
+cout << "Enter second number: "; 
+cin >> num2; 
+int sum = num1 + num2; int product = num1 * num2; 
+int difference = num1 - num2; 
+int quotient = num1 / num2;
+int remainder = num1 % num2;
+cout << "sum: " << sum << endl; 
+cout << "product: " << product << endl; 
+cout << "difference: " << difference << endl; 
+cout << "quotient: "<< quotient << endl;
+cout << "remainder"<< remainder <<endl;
+
+ return 0;
+ 
+
+}
